785ed23b9b3194f16f44ceaf3db29d2bd571f404
Keunbae Park <ppotpo@gmail.com>
2019-03-07 21:26:45 -0500

    sticky-headers

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,16 @@
 <?php
 
-Route::view('/', 'netlify');
+use Faker\Generator as Faker;
+
+Route::get('/', function (Faker $faker) {
+    $index = [];
+    foreach (range(65, 80) as $key) {
+        foreach (range(0, 20) as $value) {
+            $index[$key][] = $faker->word;
+        }
+    }
+    return view('sticky-headers', [
+        'index' => $index
+    ]);
+});
+
