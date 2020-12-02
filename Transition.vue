7c8046e82df29cc3b240dfa5921fd54125d3b650
Keunbae Park <ppotpo@gmail.com>
2020-12-02 01:07:24 -0500

    wip

---
+++ Transition.vue
@@ -0,0 +1,22 @@
+<template>
+  <transition
+    enter-active-class="transition duration-1000 ease-out"
+    enter-class="transform scale-50 opacity-0 translate-x-10"
+    enter-to-class="transform scale-100 opacity-100"
+    leave-active-class="transition duration-1000 ease-out"
+    leave-class="transform scale-100 opacity-100"
+    leave-to-class="transform scale-50 opacity-0 translate-x-10"
+  >
+    <div v-if="open" class="rounded bg-red-300 p-8 shadow-lg w-40">
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    open: Boolean,
+  },
+};
+</script>
